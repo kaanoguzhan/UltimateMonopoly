a75807b1bf8cfa442a0420460fe08502c2b08fd6
mb3rk <mcukaci@ku.edu.tr>
2015-10-17 00:20:03 +0300

    no message

--- src/gui/RollingTheDice.java
+++ src/gui/RollingTheDice.java
@@ -3,7 +3,6 @@ package gui;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.ArrayList;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
