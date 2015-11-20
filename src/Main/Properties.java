15099c488c1b07c84922e220902fd9c473117842
aeak <aak@ku.edu.tr>
2015-11-20 15:01:47 +0200

    starting money -> 3200

--- src/Main/Properties.java
+++ src/Main/Properties.java
@@ -1,7 +1,7 @@
 package Main;
 
 public class Properties {
-	public static final int	START_GOLD				= 1500;
+	public static final int	START_GOLD				= 3200;
 	public static final int	BONUS_PASSING_MONEY		= 200;
 	public static final int	TOTAL_SQUARES			= 120;
 	public static final int	HEAVEN_LOCATION			= -1;
