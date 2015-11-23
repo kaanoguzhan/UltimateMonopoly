4821160b7fbc421483e7bb2589657f78a4ddb921
KeremAltay <KeremAltay>
2015-11-24 01:03:52 +0200

    chance card

---
+++ src/GameSquares/Chance/ChanceCards/MargiGras.java
@@ -0,0 +1,30 @@
+package GameSquares.Chance.ChanceCards;
+
+import java.io.Serializable;
+import GameSquares.Chance.ChanceCard;
+import Main.Player;
+import gui.AdditionalWindows.MessageDisplayer;
+
+public class MargiGras extends ChanceCard implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	public MargiGras(){
+		super();
+	}
+	
+	@Override
+	public void onDraw(Player pl) {
+		new MessageDisplayer(toString());
+		Player[] players = Main.Main.players;
+		for(int i=0;i<players.length;i++){
+			if(players[i].getLocation() != -1)
+				players[i].moveTo(49);
+		}
+	}
+
+	@Override
+	public String toString() {
+		return "You picked Margi Gras card. Every player advence to Canal Street.";
+	}
+
+}
