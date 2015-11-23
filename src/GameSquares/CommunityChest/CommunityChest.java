f7fc60a33d39d78d6a177e0caef3231b97746abd
mb3rk <mcukaci@ku.edu.tr>
2015-11-24 01:32:06 +0200

    CommunityChest Cards

--- src/GameSquares/CommunityChest/CommunityChest.java
+++ src/GameSquares/CommunityChest/CommunityChest.java
@@ -4,25 +4,25 @@ import GameSquares.GameSquare;
 import Main.Player;
 
 public class CommunityChest extends GameSquare {
-	private static final long	serialVersionUID	= 1L;
+	private static final long serialVersionUID = 1L;
 
 	public enum CommunityChestCardType {
-		BargainBusiness, RecieveConsultancyFee, RenovationSuccess
+		BargainBusiness, HouseCondemned, OnlinePricing, RecieveConsultancyFee, RenovationSuccess, StreetRepair
 	}
-	
-	private CommunityChestDeck	CommunityDeck	= null;
-	
+
+	private CommunityChestDeck CommunityDeck = null;
+
 	public CommunityChest(int id, CommunityChestDeck CommunityDeck) {
-		super(id,type.CommunityChest);
+		super(id, type.CommunityChest);
 		this.CommunityDeck = CommunityDeck;
 	}
-	
+
 	@Override
 	public void onArrive(Player pl) {
 		CommunityChestCard card = CommunityDeck.draw();
 		card.onDraw(pl);
 	}
-	
+
 	@Override
 	public String toString() {
 		return "Community Chest. Location: " + id;
