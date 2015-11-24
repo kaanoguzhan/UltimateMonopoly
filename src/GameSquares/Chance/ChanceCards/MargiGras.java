360e3d68b2e54801a5fd9c3d3ce6f1b666687f3e
KeremAltay <KeremAltay>
2015-11-24 03:20:34 +0200

    mortgageland

--- src/GameSquares/Chance/ChanceCards/MargiGras.java
+++ src/GameSquares/Chance/ChanceCards/MargiGras.java
@@ -3,6 +3,7 @@ package GameSquares.Chance.ChanceCards;
 import java.io.Serializable;
 import GameSquares.Chance.ChanceCard;
 import Main.Player;
+import Main.Properties;
 import gui.AdditionalWindows.MessageDisplayer;
 
 public class MargiGras extends ChanceCard implements Serializable {
@@ -17,14 +18,15 @@ public class MargiGras extends ChanceCard implements Serializable {
 		new MessageDisplayer(toString());
 		Player[] players = Main.Main.players;
 		for(int i=0;i<players.length;i++){
-			if(players[i].getLocation() != -1)
+			if((players[i].getLocation() != Properties.HEAVEN_LOCATION)
+					|| !players[i].isJailed())
 				players[i].moveTo(49);
 		}
 	}
 
 	@Override
 	public String toString() {
-		return "You picked Margi Gras card. Every player advence to Canal Street.";
+		return "You picked Margi Gras card. Every player advance to Canal Street.";
 	}
 
 }
