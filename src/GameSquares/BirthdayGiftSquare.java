37555d0373776094dab1ffc13e243b6bd5686113
KeremAltay <KeremAltay>
2015-12-25 21:01:42 +0200

    birthday

--- src/GameSquares/BirthdayGiftSquare.java
+++ src/GameSquares/BirthdayGiftSquare.java
@@ -1,24 +1,31 @@
 package GameSquares;
 
 import Main.Player;
+import gui.AdditionalWindows.InputReaders.GetYesNoInput;
 
 public class BirthdayGiftSquare extends GameSquare {
 	private static final long	serialVersionUID	= 1L;
 	
+	private final int giftMoney = 100;
+	private final int nearestCabId = 46;
+	
 	public BirthdayGiftSquare(int id) {
 		super(id, type.BirthdayGift);
-		// TODO Auto-generated constructor stub
 	}
 	
 	@Override
 	public void onArrive(Player pl) {
-		// TODO Auto-generated method stub
+		boolean birthdayGiftChoice = new GetYesNoInput("You can get "+giftMoney+"$ or go to the nearest cab", 
+        		"Do you want get the "+giftMoney+"$?").getValue();
 		
+		if(birthdayGiftChoice)
+			pl.addMoney(giftMoney);
+		else
+			pl.moveTo(nearestCabId);
 	}
 	
 	@Override
 	public String toString() {
-		// TODO Auto-generated method stub
 		return "Bithday Gift Square";
 	}
 	
