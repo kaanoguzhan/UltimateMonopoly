a872ad4db3cddef59b909cb8029ed03ce95ad816
mb3rk <mcukaci@ku.edu.tr>
2015-12-26 22:33:50 +0200

    CARDI CARD COMMENTED

--- src/GameSquares/Cards/CommunityChestDeck.java
+++ src/GameSquares/Cards/CommunityChestDeck.java
@@ -8,24 +8,23 @@ import GameSquares.Cards.CommunityChestCards.OnlinePricing;
 import GameSquares.Cards.CommunityChestCards.VehicleImpounded;
 
 public class CommunityChestDeck implements Serializable, Deck {
-	private static final long serialVersionUID = 1L;
-	private CommunityChestCard[] cards = null;
-
-	public CommunityChestDeck() {
-		CommunityChestCard[] cards = { // new BargainBusiness(),
-		 //new HouseCondemned(),
-				new OnlinePricing(),
-		// new RecieveConsultancyFee(), new RenovationSuccess(),
-		// new StreetRepair(),
-				new VehicleImpounded(), new GeneralRepairs(), new InheritStock() };
-		this.cards = cards;
-	}
-
-	public CommunityChestCard draw() {
-		return cards[new Random().nextInt(cards.length)];
-	}
-
-	public CommunityChestCard[] getDeck() {
-		return cards;
-	}
+    private static final long    serialVersionUID = 1L;
+    private CommunityChestCard[] cards            = null;
+    
+    public CommunityChestDeck() {
+        CommunityChestCard[] cards = { // new BargainBusiness(),
+        // new HouseCondemned(),
+        // new RecieveConsultancyFee(), new RenovationSuccess(),
+        // new StreetRepair(),
+                new OnlinePricing(), new VehicleImpounded(), new GeneralRepairs(), new InheritStock() };
+        this.cards = cards;
+    }
+    
+    public CommunityChestCard draw() {
+        return cards[new Random().nextInt(cards.length)];
+    }
+    
+    public CommunityChestCard[] getDeck() {
+        return cards;
+    }
 }
