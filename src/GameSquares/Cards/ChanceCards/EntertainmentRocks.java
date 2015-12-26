1334ff3cd9bff1181ab392088db07e6509482ea2
mb3rk <mcukaci@ku.edu.tr>
2015-12-26 22:23:37 +0200

    Implementation of stock related Cards done

--- src/GameSquares/Cards/ChanceCards/EntertainmentRocks.java
+++ src/GameSquares/Cards/ChanceCards/EntertainmentRocks.java
@@ -1,32 +1,44 @@
 package GameSquares.Cards.ChanceCards;
 
 import gui.AdditionalWindows.MessageDisplayer;
-
+import gui.Board.RollingTheDice;
 import java.io.Serializable;
-
 import GameSquares.Cards.ChanceCard;
+import GameSquares.StockExchange.stockType;
+import Main.Main;
 import Main.Player;
 
 public class EntertainmentRocks extends ChanceCard implements Serializable {
-	private static final long serialVersionUID = 1L;
-
-	public EntertainmentRocks() {
-		super(CardType.EntertainmentRocks, false);
-	}
-
-	@Override
-	public void onDraw(Player pl) {
-		new MessageDisplayer(toString());
-		//Stakeholders in Motion Pictures and General can immediately collect dividends
-	}
-
-	public String toString() {
-		return "You picked EntertainmentRocks Card.";
-	}
-
-	@Override
-	public String getName() {
-		return "Entertainment Rocks";
-	}
-
+    private static final long serialVersionUID = 1L;
+    
+    public EntertainmentRocks() {
+        super(CardType.EntertainmentRocks, false);
+    }
+    
+    @Override
+    public void onDraw(Player pl) {
+        new MessageDisplayer(toString());
+        String div = "";
+        for (Player player : Main.players) {
+            int divident = (int) (((stockType.GeneralRadIO.getPrice() / 10) * (Math.pow(2.5,
+                player.getStockAmount(stockType.GeneralRadIO)) - 1)))
+                +
+                (int) (((stockType.MotionPictures.getPrice() / 10) * (Math.pow(2.5,
+                    player.getStockAmount(stockType.MotionPictures)) - 1)));
+            
+            player.addMoney(divident);
+            div += " " + divident + "    ";
+        }
+        RollingTheDice.logAdd("Dividents paid: " + div);
+    }
+    
+    public String toString() {
+        return "You picked EntertainmentRocks Card.";
+    }
+    
+    @Override
+    public String getName() {
+        return "Entertainment Rocks";
+    }
+    
 }
--- src/GameSquares/Cards/CommunityChestCards/InheritStock.java
+++ src/GameSquares/Cards/CommunityChestCards/InheritStock.java
@@ -1,33 +1,33 @@
 package GameSquares.Cards.CommunityChestCards;
 
 import gui.AdditionalWindows.MessageDisplayer;
-
+import gui.AdditionalWindows.StockExcWindow;
 import java.io.Serializable;
-
 import GameSquares.Cards.CommunityChestCard;
 import Main.Player;
 
 public class InheritStock extends CommunityChestCard implements Serializable {
-	private static final long serialVersionUID = 1L;
-
-	public InheritStock() {
-		super(CardType.InheritStock, false);
-	}
-
-	@Override
-	public void onDraw(Player pl) {
-		new MessageDisplayer(toString());
-		//You may chose any 1 share of any unpurchased stock to add to your portfolio
-	}
-
-	public String toString() {
-		return "You picked InheritStock Card.";
-	}
-
-	@Override
-	public String getName() {
-
-		return "Inherit Stock";
-	}
-
+    private static final long serialVersionUID = 1L;
+    
+    public InheritStock() {
+        super(CardType.InheritStock, false);
+    }
+    
+    @Override
+    public void onDraw(Player pl) {
+        new MessageDisplayer(toString());
+        StockExcWindow ste = new StockExcWindow(pl, 1);
+        ste.setVisible(true);
+    }
+    
+    public String toString() {
+        return "You picked InheritStock Card.";
+    }
+    
+    @Override
+    public String getName() {
+        
+        return "Inherit Stock";
+    }
+    
 }
